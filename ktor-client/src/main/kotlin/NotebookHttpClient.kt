f0f1e19aede0940ecf8ab161dddd3118c69f99e8
Arkady.Bazhanov <arkady.bazhanov@gmail.com>
2024-02-23 14:10:28 +0100

    Copy documentation to from ktor HttpClient and HttpResponse to their wrappers

--- ktor-client/src/main/kotlin/NotebookHttpClient.kt
+++ ktor-client/src/main/kotlin/NotebookHttpClient.kt
@@ -2,6 +2,7 @@ package org.jetbrains.kotlinx.jupyter.ktor.client
 
 import io.ktor.client.*
 import io.ktor.client.engine.*
+import java.util.ServiceLoader
 
 /**
  * A wrapper around [HttpClient] ([ktorClient]).
@@ -13,16 +14,33 @@ public class NotebookHttpClient(public val ktorClient: HttpClient) {
         userConfig: HttpClientConfig<out HttpClientEngineConfig> = HttpClientConfig()
     ) : this(HttpClient(engine, userConfig))
 
+    /**
+     * Constructs an asynchronous [NotebookHttpClient] using optional [block] for configuring this client.
+     *
+     * The [HttpClientEngine] is selected from the dependencies using [ServiceLoader].
+     * The first found implementation that provides [HttpClientEngineContainer] service implementation is used.
+     * An exception is thrown if no implementations found.
+     *
+     * See https://ktor.io/docs/http-client-engines.html
+     */
     public constructor(block: HttpClientConfig<*>.() -> Unit = {}) : this(HttpClient(block))
 }
 
+/**
+ * Creates an asynchronous [NotebookHttpClient] with the specified [HttpClientEngineFactory] and optional [block]
+ * configuration.
+ * Note that a specific platform may require a specific engine for processing requests.
+ * You can learn more about available engines from [Engines](https://ktor.io/docs/http-client-engines.html).
+ */
 public fun <T : HttpClientEngineConfig> NotebookHttpClient(
     engineFactory: HttpClientEngineFactory<T>,
     block: HttpClientConfig<T>.() -> Unit = {}
 ): NotebookHttpClient = NotebookHttpClient(HttpClient(engineFactory, block))
 
+/**
+ * Returns a new [NotebookHttpClient] by copying this client's configuration
+ * and additionally configured by the [block] parameter.
+ */
 public fun NotebookHttpClient.config(block: HttpClientConfig<*>.() -> Unit): NotebookHttpClient {
     return NotebookHttpClient(ktorClient.config(block))
 }
-
-
--- ktor-client/src/main/kotlin/NotebookHttpResponse.kt
+++ ktor-client/src/main/kotlin/NotebookHttpResponse.kt
@@ -26,10 +26,42 @@ public class NotebookHttpResponse(public val ktorResponse: HttpResponse) : HttpR
     override val status: HttpStatusCode get() = ktorResponse.status
     override val version: HttpProtocolVersion get() = ktorResponse.version
 
+    /**
+     * Tries to receive the payload of the response as a specific type [T].
+     *
+     * @throws NoTransformationFoundException If no transformation is found for the type [T].
+     * @throws DoubleReceiveException If already called [body].
+     */
     public inline fun <reified T> body(): T = runBlocking { ktorResponse.body() }
+
+    /**
+     * Tries to receive the payload of the response as a specific type [T] described in [typeInfo].
+     *
+     * @throws NoTransformationFoundException If no transformation is found for the type info [typeInfo].
+     * @throws DoubleReceiveException If already called [body].
+     */
     public fun <T> body(typeInfo: TypeInfo): T = runBlocking { ktorResponse.body(typeInfo) }
+
+    /**
+     * Reads the [HttpResponse.content] as a String. You can pass an optional [fallbackCharset]
+     * to specify a charset in the case no one is specified as part of the `Content-Type` response.
+     * If no charset specified either as parameter or as part of the response,
+     * [io.ktor.client.plugins.HttpPlainText] settings will be used.
+     *
+     * Note that [fallbackCharset] parameter will be ignored if the response already has a charset.
+     *      So it just acts as a fallback, honoring the server preference.
+     */
     public fun bodyAsText(fallbackCharset: Charset = Charsets.UTF_8): String =
         runBlocking { ktorResponse.bodyAsText(fallbackCharset) }
+
+    /**
+     * Reads the whole [HttpResponse.content] if `Content-Length` is specified.
+     * Otherwise, it just reads one byte.
+     */
     public fun readBytes(): ByteArray = runBlocking { ktorResponse.readBytes() }
+
+    /**
+     * Reads exactly [count] bytes of the [HttpResponse.content].
+     */
     public fun readBytes(count: Int): ByteArray = runBlocking { ktorResponse.readBytes(count) }
 }
