b8de9bf44323a9b922641b4b274e571bfacbd05a
Christian Melchior <christian.melchior@jetbrains.com>
2024-09-23 08:23:43 +0200

    Add support for easy conversion of http response to a dataframe

---
+++ ktor-client-core/src/main/kotlin/ext.kt
@@ -0,0 +1,24 @@
+package org.jetbrains.kotlinx.jupyter.ktor.client.core
+
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.io.readJsonStr
+
+/**
+ * If the server returned a JSON response, it can be converted directly to a Kotlin DataFrame using this method.
+ *
+ * @throws IllegalStateException if the server didn't respond with a `Content-Type: application/json` header.
+ * @throws ClassNotFoundException if Kotlin DataFrames are not on the classpath. It can be added using `%use dataframe`.
+ */
+public fun NotebookHttpResponse.toDataFrame(): DataFrame<*> {
+    return runBlocking {
+        if (ktorResponse.contentType() != ContentType.Application.Json) {
+            throw IllegalStateException("HTTP request did not return JSON, but ${ktorResponse.contentType()}")
+        }
+        val json = ktorResponse.bodyAsText()
+        DataFrame.readJsonStr(json)
+    }
+}
--- ktor-client-core/src/test/kotlin/KtorClientCoreIntegrationTest.kt
+++ ktor-client-core/src/test/kotlin/KtorClientCoreIntegrationTest.kt
@@ -2,6 +2,8 @@ package org.jetbrains.kotlinx.jupyter.ktor.client.core
 
 import kotlinx.serialization.json.JsonElement
 import org.intellij.lang.annotations.Language
+import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.jupyter.repl.result.EvalResultEx
 import org.jetbrains.kotlinx.jupyter.testkit.JupyterReplTestCase
 import org.jetbrains.kotlinx.jupyter.testkit.ReplProvider
 import org.junit.jupiter.api.Test
@@ -92,5 +94,73 @@ class KtorClientCoreIntegrationTest : JupyterReplTestCase(
         val response5 = execRaw("""client.get("https://example.org").readBytes()""")
         assertIs<ByteArray>(response5)
         assertEquals(json, response5.toString(Charsets.UTF_8))
+    }
+
+    @Test
+    fun `create dataframe from response`() {
+        val json = """[{"a": 1}, {"a": 2}, {"a": 3}]"""
+        execRaw(
+            """
+                %use serialization
+                @file:DependsOn("io.ktor:ktor-client-mock-jvm:2.3.7")
+                
+                import io.ktor.client.engine.mock.*
+                import io.ktor.client.plugins.contentnegotiation.*
+                import io.ktor.http.*
+                import io.ktor.serialization.kotlinx.json.*
+                
+                @Serializable
+                data class A(val b: String, val a: A?)
+                
+                val client = NotebookHttpClient(MockEngine) {
+                    install(ContentNegotiation) {
+                        json()
+                    }
+                    engine {
+                        addHandler {
+                            respond(
+                                content = ""${'"'}$json""${'"'},
+                                status = HttpStatusCode.OK,
+                                headers = headersOf(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                            )
+                        }
+                    }
+                }
+            """.trimIndent()
+        )
+        val response = execRaw("""client.get("https://example.org").toDataFrame()""")
+        assertIs<DataFrame<*>>(response)
+    }
+
+    @Test
+    fun `cannot create dataframe from response that doesn't return json`() {
+        execRaw(
+            """
+                %use serialization
+                @file:DependsOn("io.ktor:ktor-client-mock-jvm:2.3.7")
+                
+                import io.ktor.client.engine.mock.*
+                import io.ktor.client.plugins.contentnegotiation.*
+                import io.ktor.http.*
+                import io.ktor.serialization.kotlinx.json.*
+                
+                val client = NotebookHttpClient(MockEngine) {
+                    install(ContentNegotiation) {
+                        json()
+                    }
+                    engine {
+                        addHandler {
+                            respond(
+                                content = ""${'"'}error""${'"'},
+                                status = HttpStatusCode.InternalServerError,
+                            )
+                        }
+                    }
+                }
+            """.trimIndent()
+        )
+        val res = execEx("""client.get("https://example.org").toDataFrame()""")
+        assertIs<EvalResultEx.Error>(res)
+        assertIs<IllegalStateException>(res.error.cause)
     }
 }
