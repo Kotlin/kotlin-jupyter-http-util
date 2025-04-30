9f07afbffcbc4d3b100a2db88faa1481895fe217
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2025-04-30 22:40:23 +0200

    Add Maven Central publishing

--- buildSrc/src/main/kotlin/publisher.gradle.kts
+++ buildSrc/src/main/kotlin/publisher.gradle.kts
@@ -1,9 +1,20 @@
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.plugins.signing.SigningExtension
+
 plugins {
     `maven-publish`
+    id("signing")
 }
 
 val spaceUsername: String = System.getenv("SPACE_USERNAME") ?: ""
 val spaceToken: String = System.getenv("SPACE_TOKEN") ?: ""
+
+// Maven Central publishing properties
+val sonatypeUsername: String = System.getenv("SONATYPE_USER") ?: ""
+val sonatypePassword: String = System.getenv("SONATYPE_PASSWORD") ?: ""
+val signingKey: String? = System.getenv("SIGN_KEY_ID")
+val signingPrivateKey: String? = System.getenv("SIGN_KEY_PRIVATE")
+val signingKeyPassphrase: String? = System.getenv("SIGN_KEY_PASSPHRASE")
 
 publishing {
     publications {
@@ -14,6 +25,35 @@ publishing {
 
             artifactId = "kotlin-jupyter-${project.name}"
             from(components["kotlin"])
+
+            // POM information required by Maven Central
+            pom {
+                name.set("HTTP Utilities for Kotlin Jupyter kernel")
+                description.set("HTTP Utilities for Kotlin Jupyter: Ktor client and serialization")
+                url.set("https://github.com/Kotlin/kotlin-jupyter-http-util")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("kotlin-jupyter-team")
+                        name.set("Kotlin Jupyter Team")
+                        organization.set("JetBrains")
+                        organizationUrl.set("https://www.jetbrains.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Kotlin/kotlin-jupyter-http-util.git")
+                    developerConnection.set("scm:git:ssh://github.com/Kotlin/kotlin-jupyter-http-util.git")
+                    url.set("https://github.com/Kotlin/kotlin-jupyter-http-util")
+                }
+            }
         }
     }
 
@@ -25,5 +65,22 @@ publishing {
                 password = spaceToken
             }
         }
+
+        // Maven Central repository via Sonatype OSSRH
+        maven {
+            name = "sonatype"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = sonatypeUsername
+                password = sonatypePassword
+            }
+        }
+    }
+}
+
+if (signingKey != null) {
+    extensions.configure<SigningExtension>("signing") {
+        useInMemoryPgpKeys(signingKey, signingPrivateKey, signingKeyPassphrase)
+        sign(extensions.getByType<PublishingExtension>().publications["maven"])
     }
 }
